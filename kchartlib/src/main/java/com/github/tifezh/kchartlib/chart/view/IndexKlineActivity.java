f7f731843f1836c5403bc4cd251ba7a6a8536a8b
jiazhen <503675949@qq.com>
2020-04-01 14:45:19 +0800

    k线

---
+++ kchartlib/src/main/java/com/github/tifezh/kchartlib/chart/view/IndexKlineActivity.java
@@ -0,0 +1,17 @@
+package com.github.tifezh.kchartlib.chart.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.tifezh.kchartlib.R;
+
+public class IndexKlineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_index_kline);
+    }
+}
